7d6607112ab388b332fa44741b4b51071a866e07
x-mfe <125447515+x-mfe@users.noreply.github.com>
2023-04-27 08:59:52 +0300

    :+1:

---
+++ guessinggame/guessinggame.js
@@ -0,0 +1,26 @@
+// Arvotaan satunnaisluku 1 ja 100 välillä
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+// Alustetaan arvausten määrä 0:aan
+var guesses = 0;
+
+
+function checkGuess() {
+
+    var guess = document.getElementById("guess").value;
+
+    if (guess == randomNumber) {
+        // Jos arvaus on oikein, näytetään onnitteluviesti ja arvausten määrä
+        document.getElementById("message").innerHTML = "Congratulations! you guessed the number in" + guesses + " guesses.";
+    }  else if (guess > randomNumber) {
+        // Jos arvaus on liian suuri, pyydetään käyttäjää arvaamaan pienempää lukua.
+        document.getElementById("message").innerHTML = "Too high! Try again.";
+        // Kasvatetaan arvausten määrä yhdellä
+        guesses++;
+    } else if (guess < randomNumber) {
+        //Jos arvaus on liian pieni, pyydetään käyttäjää arvaamaan suurempaa lukua.
+        document.getElementById("message").innerHTML = "Too low! Try again.";
+        guesses++;
+    }
+
+    document.getElementById("guess").value = "";
+}
